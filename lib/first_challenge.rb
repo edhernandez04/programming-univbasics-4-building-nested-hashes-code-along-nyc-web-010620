1b81ecdf9ed1c66464d1c1b4c11b1878687eb4d3
Eddie Hernandez <mchh.004@gmail.com>
2019-12-30 16:13:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -7,10 +7,10 @@ def first_challenge
        :hero_friends => []
      }
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
+       :patriarch => {:name => "Lord Capulet", :age => 50},
+       :matriarch => {:name => "Lady Capulet", :age => 51},
+       :heroine => {:name => "Juliet", :age => 15, :status => "alive"},
+       :heroine_friends => []
      }
    }
 end
